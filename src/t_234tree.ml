4d3ed88a1d851547c84ad9ce02d75547c87e26e3
ylacaze <72870976+ylacaze@users.noreply.github.com>
2023-01-11 10:13:07 +0100

    Question 6 et 7 partie 2

--- src/t_234tree.ml
+++ src/t_234tree.ml
@@ -1,5 +1,4 @@
 
-
 (** Question 3 
     1)
     **)
@@ -21,7 +20,9 @@ module type Bt_SIG =
     val t234_search : 'a  * 'a t_234tree -> bool (*  Question 4 *)
       
     (* Question 6 *)
-    val bt_add : 'a * 'a t_234tree -> 'a t_234tree  
+    val bt_add : 'a * 'a t_234tree -> 'a t_234tree
+
+    (*val to_rb : 'a t_234tree -> 'a t_rbtree*)
 
   end
   
@@ -85,8 +86,8 @@ module BtSum : Bt_SIG =
             (match l with
             | Empty -> Rooted_2(a, x, Empty, Empty, Empty)
             | Rooted_3(x1, x2, x3, l2, m2, n2, r2) -> 
-                if a < x2 then Rooted_2(x2, x, bt_add(a,Rooted_1(x1,l2,m2)), Rooted_1(x2,n2,r2), r)
-                else Rooted_2(x2, x, Rooted_1(x1,l2,m2), bt_add(a,Rooted_1(x2,n2,r2)), r)
+                if a < x2 then Rooted_2(x2, x, bt_add(a,Rooted_1(x1,l2,m2)), Rooted_1(x3,n2,r2), r)
+                else Rooted_2(x2, x, Rooted_1(x1,l2,m2), bt_add(a,Rooted_1(x3,n2,r2)), r)
             | _ -> Rooted_1(x,bt_add(a,l),r))
             
             
@@ -129,6 +130,17 @@ module BtSum : Bt_SIG =
           else Rooted_1(x2,Rooted_1(x1,l,m),bt_add(a,Rooted_1(x3,n,r)))
           in add_aux t
     ;;
+
+        
+    
+    (* Question 6 *)
+    (*let rec to_rb(tree : 'a t_234tree) : 'a t_rbtree = 
+      match tree with
+      | Empty -> rbt_empty()
+      | Rooted_1(v, l, r) -> node(black(),v,to_rb(l),to_rb(r))
+      | Rooted_2(v,v2, l,m, r) -> node(black(),v2,node(red(),v,to_rb(l),to_rb(m)),to_rb(r))
+      | Rooted_3(v,v2,v3, l,m,n, r) -> node(black(),v2,node(red(),v,to_rb(l),to_rb(m)),node(red(),v3,to_rb(n),to_rb(r)))
+     ;;*)
 
   end
 ;;
@@ -212,7 +224,9 @@ module type RBT_SIG =
     val rbt_balance : color * 'a * 'a t_rbtree * 'a t_rbtree -> 'a t_rbtree (*Question 3*)
       
     (* Question 6 *)
-    val t_rbtree_insert : 'a * 'a t_rbtree -> 'a t_rbtree  
+    val t_rbtree_insert : 'a * 'a t_rbtree -> 'a t_rbtree
+
+    val to_234 : 'a t_rbtree -> 'a t_234tree
 
   end
   
@@ -299,10 +313,196 @@ B pour Black et R pour Red
       | Node (_, v,l, r) -> Node (Black, v, l, r)
       | Leaf -> failwith "impossible d'insérer la valeur"
     ;;
+
+
+    let rec to_234(tree : 'a t_rbtree) : 'a t_234tree =
+      
+      match tree with
+      | Leaf -> bt_empty()
+      | Node(c, v, l, r) ->
+         if rbt_isempty(l) && rbt_isempty(r) then rooted_1(v,bt_empty(),bt_empty())
+         else
+           if rbt_isempty(l) then
+             match r with
+             | Node(c2,v2,l2,r2) ->
+                if c = Red then rooted_2(v,v2,bt_empty(),to_234(l2),to_234(r2))
+                else rooted_1(v,bt_empty(),to_234(r))
+             | _ ->failwith "impossible d'arrivé la"
+           else
+             if rbt_isempty(r) then
+               match l with
+               | Node(c2,v2,l2,r2) ->
+                  if c = Red then rooted_2(v2,v,to_234(l2),to_234(r2),bt_empty())
+                  else rooted_1(v,to_234(l),bt_empty())
+               | _ ->failwith "impossible d'arrivé la"
+             else
+               match l with
+               | Node(cl,vl,ll,rl) ->
+                  (match r with
+                     | Node(cr,vr,lr,rr) ->
+                        if cl = Red then
+                          if cr = Red then
+                            rooted_3(vl,v,vr,to_234(ll),to_234(rl),to_234(lr),to_234(rr))
+                          else
+                            rooted_2(vl,v,to_234(ll),to_234(rl),to_234(r))
+                        else
+                          if cr = Red then
+                            rooted_2(v,vr,to_234(l),to_234(lr),to_234(rr))
+                          else
+                            rooted_1(v,to_234(l),to_234(r))
+                     | _ ->failwith "impossible d'arrivé la")
+               | _ ->failwith "impossible d'arrivé la"
   end
 ;;
 
 open RBtSum;;
+
+
+module type Bt_SIG =
+  sig
+    type 'a t_234tree
+
+    val rooted_1 : 'a * 'a t_234tree * 'a t_234tree -> 'a t_234tree
+    val rooted_2 : 'a * 'a *  'a t_234tree * 'a t_234tree * 'a t_234tree -> 'a t_234tree
+    val rooted_3 : 'a * 'a * 'a * 'a t_234tree * 'a t_234tree * 'a t_234tree * 'a t_234tree -> 'a t_234tree
+
+    val bt_empty : unit -> 'a t_234tree
+      
+    val bt_root : 'a t_234tree -> 'a list
+      
+    val bt_isempty : 'a t_234tree -> bool
+
+    val t234_search : 'a  * 'a t_234tree -> bool (*  Question 4 *)
+      
+    (* Question 6 *)
+    val bt_add : 'a * 'a t_234tree -> 'a t_234tree
+
+    val to_rb : 'a t_234tree -> 'a t_rbtree
+
+  end
+  
+;;
+
+module BtSum : Bt_SIG =
+  struct
+    type 'a t_234tree = Empty
+                      | Rooted_1 of 'a * 'a t_234tree * 'a t_234tree
+                      | Rooted_2 of 'a * 'a *  'a t_234tree * 'a t_234tree * 'a t_234tree
+                      | Rooted_3 of 'a * 'a * 'a * 'a t_234tree * 'a t_234tree * 'a t_234tree * 'a t_234tree ;;
+
+    let rooted_1(v, l, r: 'a* 'a t_234tree * 'a t_234tree) = Rooted_1(v, l, r)
+    let rooted_2(v,v2, l, m, r: 'a* 'a * 'a t_234tree * 'a t_234tree * 'a t_234tree) = Rooted_2(v, v2, l, m, r)
+    let rooted_3(v,v2, v3, l, m, n, r: 'a * 'a * 'a * 'a t_234tree * 'a t_234tree * 'a t_234tree * 'a t_234tree) = Rooted_3(v, v2, v3, l, m, n, r)
+    
+    let bt_empty() : 'a t_234tree =
+      Empty
+    ;;
+
+    let bt_root(bt1 : 'a t_234tree) : 'a list =
+      match bt1 with
+      |Empty -> failwith("Empty tree")
+      |Rooted_1(a,_, _) -> [a]
+      |Rooted_2(a,a2,_,_,_) -> [a; a2]
+      |Rooted_3(a,a2,a3,_, _,_,_) -> [a; a2; a3]
+    ;;
+    
+    let bt_isempty(bt1 : 'a t_234tree) : bool =
+      match bt1 with
+      |Empty -> true
+      |_ -> false
+    ;;
+
+    let t234_search(e, t: 'a * 'a t_234tree) : bool =
+      let rec search_aux t = 
+        match t with
+        | Empty -> false
+        | Rooted_1(v, l, r) -> if e == v then true else if e < v then search_aux l else search_aux r
+        | Rooted_2(v, v2, l, m, r) ->
+            if e == v || e == v2 then true
+            else if e < v then search_aux l
+            else if e > v2 then search_aux r
+            else search_aux m
+        | Rooted_3(v, v2, v3, l, m, n, r) ->
+          if e == v || e == v2 || e == v3 then true
+          else if e < v then search_aux l
+          else if e > v3 then search_aux r
+          else if e > v && e > v2 then search_aux m
+          else search_aux n
+      in search_aux t
+    ;; 
+
+  
+    let rec bt_add(a, t : 'a * 'a t_234tree) : 'a t_234tree =
+      let rec add_aux t =
+        match t with
+        | Empty -> Rooted_1(a, Empty, Empty)
+        | Rooted_1(x, l, r) ->
+          if a < x then 
+            (match l with
+            | Empty -> Rooted_2(a, x, Empty, Empty, Empty)
+            | Rooted_3(x1, x2, x3, l2, m2, n2, r2) -> 
+                if a < x2 then Rooted_2(x2, x, bt_add(a,Rooted_1(x1,l2,m2)), Rooted_1(x3,n2,r2), r)
+                else Rooted_2(x2, x, Rooted_1(x1,l2,m2), bt_add(a,Rooted_1(x3,n2,r2)), r)
+            | _ -> Rooted_1(x,bt_add(a,l),r))
+            
+            
+          else 
+            (match r with
+            | Empty -> Rooted_2(x,a, Empty, Empty, Empty)
+            | Rooted_3(x1, x2, x3, l2, m2, n2, r2) -> 
+              if a < x2 then Rooted_2(x, x2, l, bt_add(a,Rooted_1(x1,l2,m2)), Rooted_1(x3,n2,r2))
+              else Rooted_2(x, x2, l, Rooted_1(x1,l2,m2), bt_add(a,Rooted_1(x3,n2,r2)))
+            
+            | _ -> Rooted_1(x,l,bt_add(a,r)))
+          
+        | Rooted_2(x1, x2, l, m, r) ->
+          if a < x1 then 
+            (match l with
+              | Empty -> Rooted_3(a,x1,x2,Empty,Empty,Empty,Empty)
+              | Rooted_3(x12, x22, x32, l2, m2, n2, r2) ->
+                if a < x22 then Rooted_3(x22,x1,x2,bt_add(a,Rooted_1(x12,l2,m2)), Rooted_1(x32,n2,r2),m,r)
+                else Rooted_3(x22, x1,x2, Rooted_1(x1,l2,m2), bt_add(a,Rooted_1(x32,n2,r2)),m,r)
+              | _ -> Rooted_2(x1, x2, bt_add(a,l), m, r))
+            
+          else if a < x2 then
+            (match m with
+              | Empty -> Rooted_3(x1,a,x2,Empty,Empty,Empty,Empty)
+              | Rooted_3(x12, x22, x32, l2, m2, n2, r2) ->
+                if a < x22 then Rooted_3(x1,x22,x2,l,bt_add(a,Rooted_1(x12,l2,m2)), Rooted_1(x32,n2,r2),r)
+                else Rooted_3(x1,x22,x2,l,Rooted_1(x1,l2,m2), bt_add(a,Rooted_1(x32,n2,r2)),r)
+              | _ -> Rooted_2(x1,x2,l,bt_add(a, m), r))
+            
+          else 
+            (match r with
+              | Empty -> Rooted_3(x1,x2,a,Empty,Empty,Empty,Empty)
+              | Rooted_3(x12, x22, x32, l2, m2, n2, r2) ->
+                if a < x2 then Rooted_3(x1,x2,x22,l,m,bt_add(a,Rooted_1(x12,l2,m2)), Rooted_1(x32,n2,r2))
+                else Rooted_3(x1,x2,x22,l,m,Rooted_1(x1,l2,m2), bt_add(a,Rooted_1(x32,n2,r2)))
+              | _ -> Rooted_2(x1,x2,l,m,bt_add(a, r)))
+            
+        | Rooted_3(x1, x2, x3, l, m, n, r) ->
+          if a < x2 then Rooted_1(x2,bt_add(a,Rooted_1(x1,l,m)),Rooted_1(x3,n,r))
+          else Rooted_1(x2,Rooted_1(x1,l,m),bt_add(a,Rooted_1(x3,n,r)))
+          in add_aux t
+    ;;
+
+        
+    
+    (* Question 6 *)
+    let rec to_rb(tree : 'a t_234tree) : 'a t_rbtree = 
+      match tree with
+      | Empty -> rbt_empty()
+      | Rooted_1(v, l, r) -> node(black(),v,to_rb(l),to_rb(r))
+      | Rooted_2(v,v2, l,m, r) -> node(black(),v2,node(red(),v,to_rb(l),to_rb(m)),to_rb(r))
+      | Rooted_3(v,v2,v3, l,m,n, r) -> node(black(),v2,node(red(),v,to_rb(l),to_rb(m)),node(red(),v3,to_rb(n),to_rb(r)))
+    ;;
+
+  end
+;;
+
+
+
+
 (* Question 2.2 *)
  let b1 : int t_rbtree = node(black(), 20, 
                             node(black(), 10, 
@@ -358,3 +558,10 @@ rbt_search(40, !test2);; (*false*)
     rbt_search(35, !test2);;
     rbt_search(10, !test2);;
     rbt_search(3, !test2);;
+    
+    
+    
+    (* test Question 6*)
+to_rb(bt_add(6,bt_add(11,bt_add(20,bt_add(42,rooted_2(5,15,bt_empty(),bt_empty(),bt_empty()))))));; 
+
+
